ef4202311935b16068ab5537db0897d31e3859f7
Valentun Sergeev <karbon4ic@gmail.com>
2017-06-25 15:46:43 +0600

    Add view pager to MySchedule fragment

--- app/src/main/java/com/valentun/smartschool/ui/fragments/DayScheduleFragment.java
+++ app/src/main/java/com/valentun/smartschool/ui/fragments/DayScheduleFragment.java
@@ -3,28 +3,30 @@ package com.valentun.smartschool.ui.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.valentun.smartschool.R;
-import com.valentun.smartschool.adapters.MyScheduleSlideAdapter;
-import com.valentun.smartschool.utils.DateUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MyScheduleFragment extends Fragment {
+public class DayScheduleFragment extends Fragment {
 
-    @BindView(R.id.viewpager) ViewPager pager;
-    @BindView(R.id.tabLayout) TabLayout tabLayout;
+    public static final String DAY_OF_WEEK_KEY = "weekDay";
+    @BindView(R.id.day_detail_text)
+    TextView textView;
+    private int weekDay;
 
-    public static MyScheduleFragment newInstance() {
-        MyScheduleFragment fragment = new MyScheduleFragment();
+    public static DayScheduleFragment newInstance(int dayOfWeek) {
+        DayScheduleFragment fragment = new DayScheduleFragment();
+
         Bundle args = new Bundle();
+        args.putInt(DAY_OF_WEEK_KEY, dayOfWeek);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -32,24 +34,22 @@ public class MyScheduleFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            weekDay = getArguments().getInt(DAY_OF_WEEK_KEY);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_my_schedule, container, false);
+        return inflater.inflate(R.layout.fragment_day_schedule, container, false);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         ButterKnife.bind(this, view);
 
-        MyScheduleSlideAdapter adapter = new MyScheduleSlideAdapter(getChildFragmentManager());
-        pager.setAdapter(adapter);
-        tabLayout.setupWithViewPager(pager);
-
-        pager.setCurrentItem(DateUtils.ViewPagerUtils.getCurrentDayPosition());
+        textView.setText(String.valueOf(weekDay));
     }
 }
---
+++ app/src/main/java/com/valentun/smartschool/utils/DateUtils.java
@@ -0,0 +1,42 @@
+package com.valentun.smartschool.utils;
+
+import java.text.DateFormatSymbols;
+import java.util.Calendar;
+
+/**
+ * Created by Valentun on 25.06.2017.
+ */
+
+public class DateUtils {
+    private static final int DAYS_IN_WEEK = 7;
+
+    public static class ViewPagerUtils {
+        public static String getWeekDayNameByPosition(int position) {
+            int index = DateUtils.convertPositionToWeekDay(position);
+            return DateFormatSymbols.getInstance().getWeekdays()[index];
+        }
+
+        public static int getCurrentDayPosition(){
+            int weekDay = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+            return DateUtils.convertWeekDayToPosition(weekDay);
+        }
+    }
+
+    private static int convertPositionToWeekDay(int position){
+        switch (position) {
+            case DAYS_IN_WEEK - 1:
+                return 1;
+            default:
+                return  position + 2;
+        }
+    }
+
+    private static int convertWeekDayToPosition(int weekDay) {
+        switch (weekDay) {
+            case Calendar.SUNDAY:
+                return DAYS_IN_WEEK - 1;
+            default:
+                return weekDay - 2;
+        }
+    }
+}
