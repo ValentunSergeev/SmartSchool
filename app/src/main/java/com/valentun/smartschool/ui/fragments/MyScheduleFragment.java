45fbcbf1939f2e94b9bc1bad23e37b365fe62a05
Valentun Sergeev <karbon4ic@gmail.com>
2017-06-25 15:46:43 +0600

    Add view pager to MySchedule fragment

--- app/src/main/java/com/valentun/smartschool/ui/fragments/MyScheduleFragment.java
+++ app/src/main/java/com/valentun/smartschool/ui/fragments/MyScheduleFragment.java
@@ -2,14 +2,25 @@ package com.valentun.smartschool.ui.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.valentun.smartschool.R;
+import com.valentun.smartschool.adapters.MyScheduleSlideAdapter;
+import com.valentun.smartschool.utils.DateUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MyScheduleFragment extends Fragment {
+
+    @BindView(R.id.viewpager) ViewPager pager;
+    @BindView(R.id.tabLayout) TabLayout tabLayout;
 
     public static MyScheduleFragment newInstance() {
         MyScheduleFragment fragment = new MyScheduleFragment();
@@ -29,4 +40,16 @@ public class MyScheduleFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_my_schedule, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ButterKnife.bind(this, view);
+
+        MyScheduleSlideAdapter adapter = new MyScheduleSlideAdapter(getChildFragmentManager());
+        pager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(pager);
+
+        pager.setCurrentItem(DateUtils.ViewPagerUtils.getCurrentDayPosition());
+    }
 }
